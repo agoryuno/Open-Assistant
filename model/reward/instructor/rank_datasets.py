e463b5035dd516b94530a596d93ec41f8a1e2a8c
Alexander Goryunov <alex.goryunov@gmail.com>
2023-01-05 16:35:24 +0200

    Typo in docstring

--- model/reward/instructor/rank_datasets.py
+++ model/reward/instructor/rank_datasets.py
@@ -15,10 +15,10 @@
     [] support additional negative samples generated from other models.
 
         For example we can use galactica-125m to generate a TLDR and assume it was
-        inferior than the human perference one
-
+        inferior than the human preference one
 
 """
+
 from dataclasses import dataclass
 from typing import Optional, Union
 
